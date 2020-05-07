60beb91ec605ff0bae2155261ce13ea4fe8ecc87
Ryan Barrus <RBarrus@1800Contacts.com>
2020-05-07 16:38:05 -0600

    switch to serving on waitress

--- runserver.py
+++ runserver.py
@@ -6,9 +6,5 @@ from os import environ
 from FlaskWebProject import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', '0.0.0.0')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5555)
